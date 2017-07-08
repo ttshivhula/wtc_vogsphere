86391de2dbbcf2df1a4be50957134c11942c46b6
Tshivhula Tshilidzi <ttshivhu@e4r9p4.jnb.42.fr>
2017-07-08 13:06:57 +0200

    Hahahaha

--- installer.sh
+++ installer.sh
@@ -1,5 +1,6 @@
+echo "Running chmod on the files..."
 chmod +x user.sh && chmod +x krb5.sh &&
-
+echo "Installing ..."
 ./user.sh && sudo ./krb5.sh &&
 
 echo 'Setup complete, you can now enjoy Vogsphere!'
